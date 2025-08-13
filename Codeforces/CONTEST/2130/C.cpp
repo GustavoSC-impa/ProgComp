c68176596d2c6a56158807b4c076e239113d3e6e
Gustavo Cardoso <al.gustavo.cardoso@impatech.org.br>
2025-08-13 11:30:41 -0300

    bah

--- Codeforces/CONTEST/2130/C.cpp
+++ Codeforces/CONTEST/2130/C.cpp
@@ -14,18 +14,18 @@ int main(){
 
         vector< pair<int,int> > S ( 3001, {0,0});
 
-        vector<int> adj[n+1]; // isso vai guardar as arestas
+        //vector<int> adj[2*n+2]; // isso vai guardar as arestas
 
         for ( int i = 1; i <= n; i++){
             ll a, b; cin >> a >> b;
             S[i] = {a, b};
 
-            adj[a].push_back(b);
-            adj[b].push_back(a);
+            //adj[a].push_back(b);
+            //adj[b].push_back(a);
 
         }
 
-        vector<int> S_linhas ( 3001, false), tinta (7000, false);
+        vector<int> S_linhas ( 3010, 0), tinta (7000, 0);
 
         vector<int> v ( 7000, 0); // visitados com int
 
@@ -36,19 +36,40 @@ int main(){
 
             bool add = false;
 
-            for ( int idx = S[i].first; idx <= S[i].second; idx++){
+            int a = S[i].first, b = S[i].second;
+
+            for ( int idx = a; idx < b; idx++){
                 // vai percorrer a área do par
 
-                if ( tinta[idx] == false){
-                    tinta[idx] = true;
+                if ( tinta[idx] == 0){
+                    tinta[idx] = 1;
                     add = true;
-                } // se o par aumentar a área pintada, então checa se ele fecha um ciclo
-
-                int nodes = 0, edges = 0;
-
+                } // se o par aumentar a área pintada, então ele não fecha um ciclo
                 
             
             }
+            
+            /*/
+            int nodes = 0, edges = 0;
+            queue<int> q;
+            q.push(a);
+
+            while ( !q.empty()){
+                int k = q.front(); q.pop();
+                v[k] = i;
+                nodes++;
+
+                for ( int viz : adj[k]){
+                    if ( v[viz] != i){
+                        q.push(viz);
+                        edges++;
+                    }
+                }
+            }
+
+            if ( edges >= nodes ) add = false; 
+
+            //*/
 
             if ( add) ans++;
             S_linhas[i] = add;
